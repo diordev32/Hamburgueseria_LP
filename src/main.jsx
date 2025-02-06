0d3a3bf157fed862b549ed00a7bf1897e72e8b30
diego <diordev32@users.noreply.github.com>
2025-02-06 12:31:30 -0300

    ADD:Configuración inicial de entorno de desarrollo Vite, React, Json-Server, GitFlow

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const rootDOMElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootDOMElement);
+
+root.render(<App />);
